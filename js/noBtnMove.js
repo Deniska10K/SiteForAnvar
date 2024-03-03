25edd648aff0943e1c8529bb0a014d86320c076b
Denis <49648838+Deniska10K@users.noreply.github.com>
2024-03-03 19:48:30 +0300

    Update

--- js/noBtnMove.js
+++ js/noBtnMove.js
@@ -4,8 +4,8 @@ function nextPage() {
 
 function moveButton() {
     var noButton = document.getElementById('noButton');
-    var x = Math.random() * (window.innerWidth - noButton.offsetWidth);
-    var y = Math.random() * (window.innerHeight - noButton.offsetHeight);
+    var x = Math.random() * (window.innerWidth - noButton.offsetWidth) - 100;
+    var y = Math.random() * (window.innerHeight - noButton.offsetHeight) - 50;
     noButton.style.left = `${x}px`;
     noButton.style.top = `${y}px`;
 }
